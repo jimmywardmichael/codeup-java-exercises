31f9d7c18d0ec395a05722bd6d1b0608eeb86fe8
James Ward <jimmywardmichael@gmail.com>
2022-12-08 14:18:19 -0600

    made changes to java

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
+
+//main method shortcut
+//main(tab/enter)
+//psvm(tab/enter)
+
+//System.out.println shortcut
+//sout <<<<<<  is shortcut
