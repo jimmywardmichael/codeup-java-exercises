f83a50f1ebd40dc0694d7d8377247eaa35cd5569
James Ward <jimmywardmichael@gmail.com>
2022-12-08 16:00:53 -0600

    changes made in java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,11 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.print("Hello, world!");
+        System.out.println("Hello, world!");
+        int myFavoriteNumber = 7;
+     System.out.println(myFavoriteNumber);
+     String myString = "3.14159";
+     System.out.println(myString);
+
     }
 }
+//public static void main(String[] anyname) this is necessary for every file
