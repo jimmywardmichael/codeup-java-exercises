f8a37ce37866904fe9260622d1a5e12c60d25aa8
James Ward <jimmywardmichael@gmail.com>
2022-12-12 10:41:24 -0600

    finished syntax java exec

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,70 @@
+//import java.util.Scanner;
+//import java.util.String;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        int myFavoriteNumber = 2;
+        String myString = "Good Morning, Icon!";
+        System.out.println(myString);
+
+//        myString = 'c';
+//        myString = 3.14159;
+//        long myNumber = 3.14;
+//        System.out.println(myNumber);
+
+//        long myNumber = 123L;
+        // long cannot use decimal points
+
+//        long myNumber = 123;
+//        System.out.println(myNumber);
+
+//        float myNumber = 3.14;
+//        System.out.println(myNumber);
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+//        int class /////////
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+////        int three = (int) o;
+//        int three = (int) "three";
+
+//        int x = 4;
+//        x = x + 5;
+
+//        int x = 3;
+//        int y = 4;
+//        y = y * x;
+
+//        int x = 10;
+//        int y = 2;
+//        x = x / y;
+//        x /= y shorthand of line 44
+//        y = y - x;
+//        y -= x shorthand of line 46
+
+//        int reallyBigNumber = 2147483647;
+//        int reallyBigNumber2.MAX_VALUE = 2147483648;
+//line 50 goes past int buffer
+
+
+
+
+
+
+
+
+
+
+
     }
 }
 
 //main method shortcut
 //main(tab/enter)
 //psvm(tab/enter)
-
 //System.out.println shortcut
 //sout <<<<<<  is shortcut
