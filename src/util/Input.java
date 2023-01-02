3fc07a1fc406559e362ba70e6532da62a352cf19
James Ward <jimmywardmichael@gmail.com>
2023-01-02 14:33:37 -0600

    changes made

--- src/util/Input.java
+++ src/util/Input.java
@@ -1,5 +1,37 @@
 package util;
-
+import java.util.Scanner;
 public class Input {
-    private scanner;
+  private static Scanner scanner = new Scanner(System.in);
+
+  public static String getString(){
+      return scanner.nextLine();
+  }
+
+  public static boolean yesNo(){
+      String userInput = scanner.nextLine();
+      return userInput.equalsIgnoreCase("yes") || userInput.equalsIgnoreCase("y") || userInput.equalsIgnoreCase("si");
+  }
+  public int getInt(int min, int max){
+      System.out.printf("Enter a number between %d and %d", min, max);
+      int userNumber = scanner.nextInt();
+      if(userNumber < min || userNumber > max){
+          getInt(min, max);
+      }
+      return userNumber;
+  }
+    public static int getInt(){
+        return scanner.nextInt();
+    }
+    public static double getDouble(double min, double max){
+        System.out.printf("Enter a number between %s and %s", min, max);
+        double userNumber = scanner.nextInt();
+        if(Double.compare(userNumber, min) < 0 || Double.compare(userNumber, max) > 0){
+            getDouble(min, max);
+        }
+        return userNumber;
+    }
+
+    public static double getDouble(){
+        return scanner.nextDouble();
+    }
 }
