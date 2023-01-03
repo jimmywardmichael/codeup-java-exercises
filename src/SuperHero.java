8cb36ff3e4e5cc8e59b0b21607a18bcefff80ae6
James Ward <jimmywardmichael@gmail.com>
2023-01-03 10:49:04 -0600

    added shapes exercises

---
+++ src/SuperHero.java
@@ -0,0 +1,13 @@
+class SuperHero extends Person {
+    private String alterEgo;
+    public SuperHero(String name, String alterEgo) {
+        super(name);
+        this.alterEgo = alterEgo;
+    }
+    public String getName() {
+        return alterEgo;
+    }
+    public String getSecretIdentity() {
+        return super.getName();
+    }
+}
