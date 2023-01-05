6fcd1c4d81fb672beb3ef9492b4b3af92b6af367
James Ward <jimmywardmichael@gmail.com>
2023-01-05 15:05:00 -0600

    finish shapes

--- src/shapes/Measurable.java
+++ src/shapes/Measurable.java
@@ -3,4 +3,8 @@ package shapes;
 public interface Measurable {
     double getPerimeter();
     double getArea();
+
+    double getLength();
+
+    double getWidth();
 }
--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -2,12 +2,24 @@ package shapes;
 
 public class ShapesTest {
     public static void main(String[] args) {
-        Rectangle box1 = new Rectangle(5, 4);
-        System.out.println(box1.getPerimeter()); //18
-        System.out.println(box1.getArea()); // 20
+//        Rectangle box1 = new Rectangle(5, 4);
+//        System.out.println(box1.getPerimeter()); //18
+//        System.out.println(box1.getArea()); // 20
+//
+//        Rectangle box2 = new Square(5);
+//        System.out.println(box2.getPerimeter());//20
+//        System.out.println(box2.getArea());//25
 
-        Rectangle box2 = new Square(5);
-        System.out.println(box2.getPerimeter());//20
-        System.out.println(box2.getArea());//25
+        Measurable myShape;
+
+        myShape = new Square(5);
+        System.out.println(myShape.getPerimeter());
+        System.out.println(myShape.getArea());
+
+        myShape = new Rectangle(5 , 2);
+        System.out.println(myShape.getPerimeter());
+        System.out.println(myShape.getArea());
+        System.out.println(myShape.getLength());
+        System.out.println(myShape.getWidth());
     }
 }
