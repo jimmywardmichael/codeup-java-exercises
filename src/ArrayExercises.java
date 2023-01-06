0d0f4b03661dfbebf726c8fca6b2977964c9ee46
James Ward <jimmywardmichael@gmail.com>
2023-01-06 12:13:52 -0600

    added files for exercise

--- src/ArrayExercises.java
+++ src/ArrayExercises.java
@@ -1,2 +1,19 @@
+
+import java.util.Arrays;
 public class ArrayExercises {
+public static void main(String[] args){
+    int[] numbers = {1,2,3,4,5};
+    System.out.println(numbers);
+}
+    Person[] people = new Person[3];{
+        people[0] = new Person("Alice");
+        people[1] = new Person("Bob");
+        people[2] = new Person("Charlie");
+        for (int i = 0; i < people.length; i++) {
+            System.out.println(people[i].getName());
+    }
+
+
+    }
+
 }
