06b5da102cb4174ac898302ccc591384b4cbe588
James Ward <jimmywardmichael@gmail.com>
2023-01-08 18:20:18 -0600

    changes made

--- src/grades/Student.java
+++ src/grades/Student.java
@@ -1,4 +1,16 @@
 package grades;
 
 public class Student {
+    private String studentName;
+    private double grades;
+
+    public static void main(String[] args) {
+        // returns the student's name
+        public String getName();
+        // adds the given grade to the grades property
+        public void addGrade(int grade);
+        // returns the average of the students grades
+        public double getGradeAverage();
+    }
+
 }
